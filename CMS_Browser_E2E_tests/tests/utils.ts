1418f41ad0c9a101c78367c1d60eb55483b79d9e
unknown <mohamedali.naguez@esprit.tn>
2025-06-19 13:17:25 +0100

    edited env

--- CMS_Browser_E2E_tests/tests/utils.ts
+++ CMS_Browser_E2E_tests/tests/utils.ts
@@ -1,11 +1,15 @@
 import { Page } from '@playwright/test';
 import * as dotenv from 'dotenv';
-dotenv.config();
 
-export const TEST_EMAIL = process.env.TEST_EMAIL!;
-export const TEST_EMAILD = process.env.TEST_EMAILD!;
+dotenv.config(); // works locally; Jenkins will override via environment
 
-export const TEST_PASSWORD = process.env.TEST_PASSWORD!;
+export const TEST_EMAIL = process.env.TEST_EMAIL ?? '';
+export const TEST_PASSWORD = process.env.TEST_PASSWORD ?? '';
+
+if (!TEST_EMAIL || !TEST_PASSWORD) {
+  throw new Error("❌ TEST_EMAIL or TEST_PASSWORD not defined in environment!");
+}
+
 export async function login(page: Page, email: string, password: string) {
   await page.goto('https://dashboard.cam2drive-bt.com/login');
   await page.getByRole('textbox', { name: 'Email' }).fill(email);
