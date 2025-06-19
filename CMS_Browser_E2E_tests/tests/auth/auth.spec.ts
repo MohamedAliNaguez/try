da7bade296d812e4669387b457b986c03094a738
unknown <mohamedali.naguez@esprit.tn>
2025-06-19 16:10:27 +0100

    try fix creds

--- CMS_Browser_E2E_tests/tests/auth/auth.spec.ts
+++ CMS_Browser_E2E_tests/tests/auth/auth.spec.ts
@@ -4,10 +4,7 @@ import { login, TEST_EMAIL , TEST_PASSWORD } from '../utils';
 test('Login and save session', async ({ page }) => {
   await page.goto('https://dashboard.cam2drive-bt.com/login');
 
-
-  const em = "dalynaguez@gmail.com";
-  const  pw = "Sennanolight1*";
-  await login(page, em, pw);
+  await login(page, TEST_EMAIL, TEST_PASSWORD);
 
   // Wait for a specific element that appears only after login
   await page.waitForSelector('text=Customers', { timeout: 300000 }); // waits up to 30 seconds
